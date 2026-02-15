50149a2bba05fa8ec74238d2a102db2f9207e19d
Efe Yılmaz <iceburn322@gmail.com>
2026-02-15 21:58:25 +0300

    Testing some stuff I guess

--- .next/dev/server/chunks/_next-internal_server_app_api_trainingprograms_route_actions_58a8f449.js
+++
@@ -1,7 +0,0 @@
-module.exports = [
-"[project]/.next-internal/server/app/api/trainingprograms/route/actions.js [app-rsc] (server actions loader, ecmascript)", ((__turbopack_context__, module, exports) => {
-
-}),
-];
-
-//# sourceMappingURL=_next-internal_server_app_api_trainingprograms_route_actions_58a8f449.js.map
